4f12c5bee113b14382296a9401beed9ae647c930
han <han@qq.com>
2020-04-16 19:45:01 +0800

    第一次提交

---
+++ src/main/java/com/github/Git.java
@@ -0,0 +1,10 @@
+package com.github;
+
+/**
+ * time: 2020/4/16 0016 19:44
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("master...git");
+    }
+}
