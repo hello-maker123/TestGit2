58e37a67038b4155bf9510b035b1714d296d994e
hello-maker123 <63486896+hello-maker123@users.noreply.github.com>
2020-04-16 20:11:38 +0800

    Update Git4.java

--- src/main/java/com/github/Git4.java
+++ src/main/java/com/github/Git4.java
@@ -5,6 +5,6 @@ package com.github;
  */
 public class Git4 {
     public static void main(String[] args) {
-        System.out.println("这是git4.");
+        System.out.println("这是git4.。。。。。。。。。");
     }
 }
